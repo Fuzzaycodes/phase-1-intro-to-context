1822f2a68dc56f80300a4037307bf9bd92a877bd
Fuzzay <samomwerif2@gmail.com>
2024-07-21 17:07:50 +0300

    context

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 // Your code here
+
+
+function createEmployeeRecord(array) {
+    return {
+      firstName: array[0],
+      familyName: array[1],
+      title: array[2],
+      payPerHour: array[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  function createEmployeeRecords(array) {
+    return array.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(employeeRecord, dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    employeeRecord.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date
+    });
+    return employeeRecord;
+  }
+  
+  function createTimeOutEvent(employeeRecord, dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    employeeRecord.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date
+    });
+    return employeeRecord;
+  }
+  
+  function hoursWorkedOnDate(employeeRecord, date) {
+    const timeIn = employeeRecord.timeInEvents.find(e => e.date === date);
+    const timeOut = employeeRecord.timeOutEvents.find(e => e.date === date);
+    
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  
+  function wagesEarnedOnDate(employeeRecord, date) {
+    const hours = hoursWorkedOnDate(employeeRecord, date);
+    return hours * employeeRecord.payPerHour;
+  }
+  
+  function allWagesFor(employeeRecord) {
+    const dates = employeeRecord.timeInEvents.map(e => e.date);
+    return dates.reduce((total, date) => total + wagesEarnedOnDate(employeeRecord, date), 0);
+  }
+  
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total, employeeRecord) => total + allWagesFor(employeeRecord), 0);
+  }
+  
+  
+  
+  
+  createTimeInEvent(employeeRecords[0], "2023-07-01 0900");
+  createTimeOutEvent(employeeRecords[0], "2023-07-01 1700");
+  
+  createTimeInEvent(employeeRecords[1], "2023-07-01 1000");
+  createTimeOutEvent(employeeRecords[1], "2023-07-01 1800");
+  
+  console.log(allWagesFor(employeeRecords[0])); 
+  console.log(allWagesFor(employeeRecords[1])); 
+  console.log(calculatePayroll(employeeRecords)); 
+  
